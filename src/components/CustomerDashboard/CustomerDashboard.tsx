d674bca5e7581eaaa3f278950054995ff5ae7a73
Adrian Zabolitzki <adrianzab@gmail.com>
2023-11-28 20:15:47 -0700

    Add 3 initial components

---
+++ src/components/CustomerDashboard/CustomerDashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomerDash = () => {
+  return (
+    <h2>hi from Customer dash</h2>
+  )
+}
+
+export default CustomerDash
---
+++ src/components/FarmerDashboard/FarmerDashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const FarmerDashboard = () => {
+
+  return (
+    <h2>from FarmerDashboard</h2>
+  )
+}
+
+export default FarmerDashboard

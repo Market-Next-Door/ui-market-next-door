fe1eb181116e80785db560a4b32f63fdac19131f
Adrian Zabolitzki <adrianzab@gmail.com>
2023-11-29 20:16:14 -0700

    Create vendor login page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,11 +4,13 @@ import "./App.css";
 import LandingPage from "../LandingPage/LandingPage";
 import FarmerDashboard from "../FarmerDashboard/FarmerDashboard";
 import CustomerDash from "../CustomerDashboard/CustomerDashboard";
+import VendorLogIn from "../VendorLogIn/VendorLogIn";
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
+      <VendorLogIn />
       <FarmerDashboard />
       <CustomerDash />
     </div>
---
+++ src/components/VendorLogIn/VendorLogIn.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './VendorLogIn.css'
+
+const VendorLogIn = () => {
+
+  return (
+    <div className='login-container'>
+      <h2 className='login-header'>MARKET NEXT DOOR</h2>
+      <input className='login-input'
+        type='text'
+        placeholder='Enter email...'
+        value='Farmer Doe'
+
+      />
+      <input className='login-input'
+        type='text'
+        placeholder='Password...'
+        value='123'
+
+      />
+      <button className='login-submit-btn'>SIGN IN</button>
+      <p className='login-text'>OR</p>
+      <button className='login-submit-btn'>SIGN UP</button>
+      <button className='login-go-back-btn'>Go Back</button>
+
+    </div>
+    
+  )
+}
+
+export default VendorLogIn

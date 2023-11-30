6289987fbbb36c2d821b9bb6f6c6f29d8aec4be1
Adrian Zabolitzki <adrianzab@gmail.com>
2023-11-29 21:26:04 -0700

    Create customer sign up page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,6 +7,7 @@ import CustomerDash from "../CustomerDashboard/CustomerDashboard";
 import VendorLogIn from "../VendorLogIn/VendorLogIn";
 import VendorSignUp from "../VendorSignUp/VendorSignUp";
 import CustomerLogIn from "../CustomerLogIn/CustomerLogIn";
+import CustomerSignUp from "../CustomerSignUp/CustomerSignUp";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <VendorLogIn />
       <VendorSignUp />
       <CustomerLogIn />
+      <CustomerSignUp />
       <FarmerDashboard />
       <CustomerDash />
     </div>

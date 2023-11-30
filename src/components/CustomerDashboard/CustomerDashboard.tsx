f3ca142328566cc429fc795b4da4acbc1c4e10a9
Nicole Rue <nicolerue92@gmail.com>
2023-11-29 20:31:16 -0800

    add modal feature for the invoice

--- src/components/CustomerDashboard/CustomerDashboard.tsx
+++ src/components/CustomerDashboard/CustomerDashboard.tsx
@@ -66,6 +66,19 @@ const CustomerDash = () => {
 };
 
 const CustomerViewItemCard = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  function handlePreOrderClick() {
+    openModal();
+  }
   const [quantity, setQuantity] = useState<number>(0);
 
   const increaseQuantity = () => {
@@ -75,48 +88,145 @@ const CustomerViewItemCard = () => {
   const decreaseQuantity = () => {
     setQuantity((prevQuantity) => Math.max(prevQuantity - 1, 0));
   };
-  return (
-    <div className="customer-view-item-card">
-      <div className="vendor-item-image">
-        <img src="carrots.jpg" alt="Item" />
-      </div>
-      <div className="item-details">
-        <p>
-          Name: <span>Carrots</span>
-        </p>
-        <p>
-          Size: <span>5lb</span>
-        </p>
-        <p>
-          Details: <span>Small, crooked carrots</span>
-        </p>
 
-        <p>
-          Quantity Available: <span>47</span>
-        </p>
-        <div className="quantity-input">
-          <button className="quantity-btn" onClick={decreaseQuantity}>
-            -
-          </button>
-          <input
-            className="quantity-num"
-            value={quantity}
-            onChange={(e) =>
-              setQuantity(Math.max(0, parseInt(e.target.value) || 0))
-            }
-          />
-          <button className="quantity-btn" onClick={increaseQuantity}>
-            +
+  const [creditCard, setCreditCard] = useState({
+    cardNumber: "",
+    expirationDate: "",
+    cvv: "",
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setCreditCard((prev) => ({ ...prev, [name]: value }));
+  };
+  return (
+    <>
+      <div className="customer-view-item-card">
+        <div className="vendor-item-image">
+          <img src="carrots.jpg" alt="Item" />
+        </div>
+        <div className="item-details">
+          <p>
+            Name: <span>Carrots</span>
+          </p>
+          <p>
+            Size: <span>5lb</span>
+          </p>
+          <p>
+            Details: <span>Small, crooked carrots</span>
+          </p>
+
+          <p>
+            Quantity Available: <span>47</span>
+          </p>
+          <div className="quantity-input">
+            <button className="quantity-btn" onClick={decreaseQuantity}>
+              -
+            </button>
+            <input
+              className="quantity-num"
+              value={quantity}
+              onChange={(e) =>
+                setQuantity(Math.max(0, parseInt(e.target.value) || 0))
+              }
+            />
+            <button className="quantity-btn" onClick={increaseQuantity}>
+              +
+            </button>
+          </div>
+          <button className="purchase-btn" onClick={handlePreOrderClick}>
+            Pre-order
           </button>
         </div>
-        <button
-          className="purchase-btn"
-          onClick={() => console.log("Purchased!")}
-        >
-          Pre-order
-        </button>
       </div>
-    </div>
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <h2 className="invoice-header">Order Confirmation</h2>
+            <p className="invoice-invoice-num">
+              <strong>INVOICE #:</strong> 12
+            </p>
+            <div className="invoice-info">
+              <p>
+                <strong>Vendor:</strong> Brian's Potatoes
+              </p>
+              <p>
+                <strong>Vendor Contact:</strong> brianpotatoes@mail.com
+              </p>
+              <p>
+                <strong>Customer:</strong> Ryan Spyin
+              </p>
+              <p>
+                <strong>Customer Contact:</strong> ryanspyin@mail.com
+              </p>
+              <p>
+                <strong>Market:</strong> Market Next Door
+              </p>
+              <p>
+                <strong>Pick Up Date:</strong> Saturday December 12, 2023
+              </p>
+              <p>
+                <strong>Pick Up Time:</strong> 10am - 2pm
+              </p>
+              <p>
+                <strong>Item:</strong> Carrots
+              </p>
+              <p>
+                <strong>Size:</strong> 5lb
+              </p>
+              <p className="details-invoice">
+                <strong>Details:</strong> Small, crooked carrots Small, crooked
+              </p>
+              <p>
+                <strong>Price per Item:</strong> $5.00
+              </p>
+
+              <p>
+                <strong>Quantity:</strong> 2
+              </p>
+            </div>
+            <p className="invoice-total">
+              <strong>Total:</strong> $10.00{" "}
+            </p>
+            <div className="invoice-payment-info">
+              <label>Card Number:</label>
+              <input
+                className="modal-input-cc"
+                type="text"
+                name="cardNumber"
+                value={creditCard.cardNumber}
+                onChange={handleInputChange}
+              />
+              <label>Expiration Date:</label>
+              <input
+                className="modal-input-cc-ex"
+                type="text"
+                name="expirationDate"
+                value={creditCard.expirationDate}
+                onChange={handleInputChange}
+              />
+              <label>CVV:</label>
+              <input
+                className="modal-input-cc-cvv"
+                type="text"
+                name="cvv"
+                value={creditCard.cvv}
+                onChange={handleInputChange}
+              />
+            </div>
+            <button
+              className="order-confirm-btn"
+              onClick={() => alert("Order Confirmed!")}
+            >
+              Confirm Order
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -11,6 +11,89 @@ const LandingPage = () => {
         <button className="landing-btn">Vendor</button>
         <button className="landing-btn">Customer</button>
       </div>
+      <p className="landing-tagline">HOW IT WORKS </p>
+      <div className="how-it-works-steps">
+        <div className="how-it-works-step">
+          <p>VENDORS WILL UPDATE INVENTORY ON MON</p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="how-it-works-svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+            />
+          </svg>
+        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="how-it-works-svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+          />
+        </svg>
+
+        <div className="how-it-works-step">
+          <p>PLACE AN ORDER BETWEEN TUES-THURS</p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="how-it-works-svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+            />
+          </svg>
+        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="how-it-works-svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+          />
+        </svg>
+        <div className="how-it-works-step">
+          <p>PICK UP YOUR ORDER ON SAT @ THE MARKET NEXT DOOR</p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="how-it-works-svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
+            />
+          </svg>
+        </div>
+      </div>
     </div>
   );
 };

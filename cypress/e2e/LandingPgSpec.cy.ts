d4c44585a12d871de3424614ab032e1dbc4750c9
Nicole Rue <nicolerue92@gmail.com>
2023-12-01 11:04:17 -0800

    change url

--- cypress/e2e/LandingPgSpec.cy.ts
+++ cypress/e2e/LandingPgSpec.cy.ts
@@ -1,6 +1,6 @@
 describe("should load landing page elements", () => {
   it("should display landing page elements", () => {
-    cy.visit("http://localhost:3000");
+    cy.visit("https://market-next-door-fe-f6728ad38b62.herokuapp.com/");
     cy.get(".landing-tagline").should("contain", "Community");
     cy.get(".landing-header").should("have.text", "MARKET NEXT DOOR");
   });

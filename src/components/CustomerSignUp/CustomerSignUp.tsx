8a95e78a7fd548fb8b68840929a0f309c9f81c93
Adrian Zabolitzki <adrianzab@gmail.com>
2023-11-30 20:13:28 -0700

    Add functions for input onChange, submitCustomer

--- src/components/CustomerSignUp/CustomerSignUp.tsx
+++ src/components/CustomerSignUp/CustomerSignUp.tsx
@@ -1,40 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CustomerSignUp.css'
 
 const CustomerSignUp = () => {
+  const [customerFirstName, setCustomerFirstName] = useState('')
+  const [customerLastName, setCustomerLastName] = useState('')
+  const [customerEmail, setCustomerEmail] = useState('')
+  const [customerPassword, setCustomerPassword] = useState('')
+  const [customerPasswordMatch, setCustomerPasswordMatch] = useState('')
 
+  const submitCustomer = () => {
+    const newCustomer = {
+      customerFirstName,
+      customerLastName,
+      customerPassword,
+      customerPasswordMatch,
+    }
+  }
   return (
     
-    <div className='sign-up-container'>
-      <h2 className='sign-up-header'>MARKET NEXT DOOR</h2>
-      <input className='sign-up-input'
+    <form className='customer-sign-up-container'>
+      <h2 className='customer-sign-up-header'>MARKET NEXT DOOR</h2>
+      <input className='customer-sign-up-input'
         type='text'
+        name='firstName'
         placeholder='FIRST NAME...'
-        value='Sue'
+        value={customerFirstName}
+        onChange={e => setCustomerFirstName(e.target.value)}
       />
-      <input className='sign-up-input'
+      <input className='customer-sign-up-input'
         type='text'
+        name='lastName'
         placeholder='LAST NAME...'
-        value='Jacobs'
+        value={customerLastName}
+        onChange={e => setCustomerLastName(e.target.value)}
       />
-      <input className='sign-up-input'
+      <input className='customer-sign-up-input'
         type='text'
+        name='customerEmail'
         placeholder='ENTER E-MAIL...'
-        value='sue@mail.com'
+        value={customerEmail}
+        onChange={e => setCustomerEmail(e.target.value)}
       />
-      <input className='sign-up-input'
+      <input className='customer-sign-up-input'
         type='text'
+        name='password'
         placeholder='ENTER PASSWORD...'
-        value='456'
+        value={customerPassword}
+        onChange={e => setCustomerPassword(e.target.value)}
       />
-      <input className='sign-up-input'
+      <input className='customer-sign-up-input'
         type='text'
+        name='passwordMatch'
         placeholder='RE-ENTER PASSWORD...'
-        value='456'
+        value={customerPasswordMatch}
+        onChange={e => setCustomerPasswordMatch(e.target.value)}
       />
-      <button className='sign-up-btn'>SIGN UP</button>
-      <button className='sign-up-go-back-btn'>Go Back</button>
-    </div>
+      <button className='customer-sign-up-btn'>SIGN UP</button>
+      <button className='customer-sign-up-go-back-btn'>Go Back</button>
+    </form>
+    
   )
 }
 

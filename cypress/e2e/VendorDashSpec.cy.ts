9d8432034e7d04fc5bc29102feef22c9440f679d
Ann Hochworter <ann.hochworter@gmail.com>
2023-12-02 11:12:46 -0700

    Fix cypress testing due to class rename

--- cypress/e2e/VendorDashSpec.cy.ts
+++ cypress/e2e/VendorDashSpec.cy.ts
@@ -3,11 +3,11 @@
 describe('should load vendor dashboard page elements', () => {
   it('should display vendor dashboard page elements', () => {
     cy.visit('https://market-next-door-fe-f6728ad38b62.herokuapp.com/');
-    cy.get('.vendor-container > .vendor-header').should(
+    cy.get('.vendor-container > .mnd-header').should(
       'have.text',
       'Welcome, Vendors and Customers!'
     );
-    cy.get('.vendor-container > .vendor-nav').should('have.length', 1);
+    cy.get('.vendor-container > .mnd-nav').should('have.length', 1);
     cy.get('.add-item-form').should('be.visible');
     cy.get('.vendor-items-display').should('be.visible');
   });

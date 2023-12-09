732a9d341baf0f7842a87b63cfa70710fdb02218
Ann Hochworter <ann.hochworter@gmail.com>
2023-12-09 12:10:22 -0700

    Add customer sign up feature

--- src/components/CustomerSignUp/CustomerSignUp.tsx
+++ src/components/CustomerSignUp/CustomerSignUp.tsx
@@ -1,31 +1,49 @@
-import React, { useState } from "react";
-import "./CustomerSignUp.css";
-import { useNavigate } from "react-router";
+import React, { useState } from 'react';
+import './CustomerSignUp.css';
+import { useNavigate } from 'react-router';
+import { postNewCustomer } from '../../apiCalls';
 
-const CustomerSignUp = () => {
+export type newCustomer = {
+  customerFirstName: string;
+  customerLastName: string;
+  customerEmail: string;
+  customerPassword: string;
+  customerPasswordMatch: string;
+};
+
+type CustomerSignUpProps = {
+  addCustomer: Function;
+};
+
+const CustomerSignUp = ({ addCustomer }: CustomerSignUpProps) => {
   const navigate = useNavigate();
 
   function handleGoBack() {
-    navigate("/");
-  }
-  function handleSignup() {
-    //post request
+    navigate('/');
   }
 
-  const [customerFirstName, setCustomerFirstName] = useState("");
-  const [customerLastName, setCustomerLastName] = useState("");
-  const [customerEmail, setCustomerEmail] = useState("");
-  const [customerPassword, setCustomerPassword] = useState("");
-  const [customerPasswordMatch, setCustomerPasswordMatch] = useState("");
-
-  const submitCustomer = () => {
+  function handleSignup(e: any) {
+    e.preventDefault();
     const newCustomer = {
-      customerFirstName,
-      customerLastName,
-      customerPassword,
-      customerPasswordMatch,
+      first_name: customerFirstName,
+      last_name: customerLastName,
+      email: customerEmail,
+      password: customerPassword,
     };
-  };
+    postNewCustomer(newCustomer)
+      .then(data => {
+        addCustomer(data);
+        console.log(data);
+      })
+      .catch(error => console.log(error));
+  }
+
+  const [customerFirstName, setCustomerFirstName] = useState('');
+  const [customerLastName, setCustomerLastName] = useState('');
+  const [customerEmail, setCustomerEmail] = useState('');
+  const [customerPassword, setCustomerPassword] = useState('');
+  const [customerPasswordMatch, setCustomerPasswordMatch] = useState('');
+
   return (
     <form className="customer-sign-up-container">
       <h2 className="customer-sign-up-header">MARKET NEXT DOOR</h2>
@@ -37,7 +55,7 @@ const CustomerSignUp = () => {
         name="firstName"
         placeholder="FIRST NAME..."
         value={customerFirstName}
-        onChange={(e) => setCustomerFirstName(e.target.value)}
+        onChange={e => setCustomerFirstName(e.target.value)}
       />
       <input
         className="customer-sign-up-input"
@@ -45,7 +63,7 @@ const CustomerSignUp = () => {
         name="lastName"
         placeholder="LAST NAME..."
         value={customerLastName}
-        onChange={(e) => setCustomerLastName(e.target.value)}
+        onChange={e => setCustomerLastName(e.target.value)}
       />
       <input
         className="customer-sign-up-input"
@@ -53,7 +71,7 @@ const CustomerSignUp = () => {
         name="customerEmail"
         placeholder="ENTER E-MAIL..."
         value={customerEmail}
-        onChange={(e) => setCustomerEmail(e.target.value)}
+        onChange={e => setCustomerEmail(e.target.value)}
       />
       <input
         className="customer-sign-up-input"
@@ -61,7 +79,7 @@ const CustomerSignUp = () => {
         name="password"
         placeholder="ENTER PASSWORD..."
         value={customerPassword}
-        onChange={(e) => setCustomerPassword(e.target.value)}
+        onChange={e => setCustomerPassword(e.target.value)}
       />
       <input
         className="customer-sign-up-input"
@@ -69,9 +87,9 @@ const CustomerSignUp = () => {
         name="passwordMatch"
         placeholder="RE-ENTER PASSWORD..."
         value={customerPasswordMatch}
-        onChange={(e) => setCustomerPasswordMatch(e.target.value)}
+        onChange={e => setCustomerPasswordMatch(e.target.value)}
       />
-      <button className="customer-sign-up-btn" onClick={handleSignup}>
+      <button className="customer-sign-up-btn" onClick={e => handleSignup(e)}>
         SIGN UP
       </button>
       <button className="customer-sign-up-go-back-btn" onClick={handleGoBack}>

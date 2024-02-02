8fa8607ed1696d66f7009fa4e6352896ac822ec6
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-02-01 21:10:39 -0600

    Reorder CRUD endpoints in the apiCalls.tsx file and add a temporary local server url value for testing

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -1,5 +1,7 @@
 import { updatedItem } from './components/VendorItemCard/VendorItemCard';
 
+// let url = "http://127.0.0.1:8000"
+
 let url =
   process.env.REACT_APP_DEV_MODE === 'deployment'
     ? process.env.REACT_APP_DEVELOPEMENT_URL
@@ -9,6 +11,7 @@ type ItemQuantity = {
   quantity: number;
 };
 
+// Vendor endpoints
 export function getAllVendors() {
   return fetch(`${url}/vendors/`).then(response => {
     if (!response.ok) {
@@ -18,8 +21,25 @@ export function getAllVendors() {
   });
 }
 
-export function getAllCustomers() {
-  return fetch(`${url}/customers/`).then(response => {
+export function postNewVendor(newVendor: any) {
+  return fetch(`${url}/vendors/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newVendor),
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+      return response;
+    })
+    .then(response => response.json());
+}
+
+export function getOneVendor(vendorID: number) {
+  return fetch(`${url}/vendors/${vendorID}`).then(response => {
     if (!response.ok) {
       throw new Error(`${response.status} ${response.statusText}`);
     }
@@ -27,8 +47,43 @@ export function getAllCustomers() {
   });
 }
 
-export function getAllPreOrders() {
-  return fetch(`${url}/preorders`).then(response => {
+export function updateVendorData(userID: number, updatedUserData: any) {
+  console.log('userdata look', updatedUserData);
+  return fetch(`${url}/vendors/${userID}/`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updatedUserData),
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .catch(error => console.log(error));
+}
+
+export function deleteVendor(vendorID: number) {
+  return fetch(`${url}/vendors/${vendorID}/`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+      return response;
+    })
+    .catch(error => console.log(error));
+}
+
+// Vendor-Preorder endpoint
+export function getSelectedVendorOrders(id: number) {
+  return fetch(`${url}/vendors/${id}/preorders/`).then(response => {
     if (!response.ok) {
       throw new Error(`${response.status} ${response.statusText}`);
     }
@@ -36,6 +91,7 @@ export function getAllPreOrders() {
   });
 }
 
+// Vendor-Item endpoints
 export function getSelectedVendorsItems(id: number) {
   return fetch(`${url}/vendors/${id}/items/`).then(response => {
     if (!response.ok) {
@@ -64,8 +120,68 @@ export function postVendorItem(id: string, newItem: any) {
     .then(response => response.json());
 }
 
-export function getOneVendor(vendorID: number) {
-  return fetch(`${url}/vendors/${vendorID}`).then(response => {
+export function updateVendorItem(
+  vendorid: string,
+  id: number,
+  updatedItem: updatedItem
+) {
+  return fetch(`${url}/vendors/${vendorid}/items/${id}/`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updatedItem),
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+      return response;
+    })
+    .then(response => response.json());
+}
+
+export function updateItemQuantity(
+  vendorId: number,
+  itemNum: number,
+  newQuantity: ItemQuantity
+) {
+  return fetch(`${url}/vendors/${vendorId}/items/${itemNum}/`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newQuantity),
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+      return response;
+    })
+    .then(response => response.json());
+}
+
+export function deleteVendorItem(vendorid: string, id: number) {
+  return fetch(`${url}/vendors/${vendorid}/items/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+      return response.json();
+    })
+
+    .catch(error => console.log(error));
+}
+
+// Customer endpoints
+export function getAllCustomers() {
+  return fetch(`${url}/customers/`).then(response => {
     if (!response.ok) {
       throw new Error(`${response.status} ${response.statusText}`);
     }
@@ -73,8 +189,69 @@ export function getOneVendor(vendorID: number) {
   });
 }
 
+export function postNewCustomer(newCustomer: any) {
+  return fetch(`${url}/customers/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newCustomer),
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+      return response;
+    })
+    .then(response => response.json());
+}
+
 export function getOneCustomer(customerID: number) {
   return fetch(`${url}/customers/${customerID}`).then(response => {
+    if (!response.ok) {
+      throw new Error(`${response.status} ${response.statusText}`);
+    }
+    return response.json();
+  });
+}
+
+export function updateCustomerData(userID: number, updatedUserData: any) {
+  console.log('userdata look', updatedUserData);
+  return fetch(`${url}/customers/${userID}/`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updatedUserData),
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .catch(error => console.log(error));
+}
+
+export function deleteCustomer(customerID: number) {
+  return fetch(`${url}/customers/${customerID}/`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+      return response;
+    })
+    .catch(error => console.log(error));
+}
+
+// Customer-Preorder endpoints
+export function getAllPreOrders() {
+  return fetch(`${url}/preorders`).then(response => {
     if (!response.ok) {
       throw new Error(`${response.status} ${response.statusText}`);
     }
@@ -108,49 +285,8 @@ export function getSelectedCustomerOrders(id: number) {
   });
 }
 
-export function postNewCustomer(newCustomer: any) {
-  return fetch(`${url}/customers/`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(newCustomer),
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`${response.status} ${response.statusText}`);
-      }
-      return response;
-    })
-    .then(response => response.json());
-}
-export function postNewVendor(newVendor: any) {
-  return fetch(`${url}/vendors/`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(newVendor),
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`${response.status} ${response.statusText}`);
-      }
-      return response;
-    })
-    .then(response => response.json());
-}
-
-export function getSelectedVendorOrders(id: number) {
-  return fetch(`${url}/vendors/${id}/preorders/`).then(response => {
-    if (!response.ok) {
-      throw new Error(`${response.status} ${response.statusText}`);
-    }
-    return response.json();
-  });
-}
-
-export function getMarkets(zipcode: string, radius: string) {
+// Market endpoints
+export function getMarkets(zipcode:string, radius:string) {
   return fetch(
     `https://quiet-depths-54407-77a00505f51e.herokuapp.com/markets/location/${zipcode}/${radius}`
   ).then(response => {
@@ -159,131 +295,4 @@ export function getMarkets(zipcode: string, radius: string) {
     }
     return response.json();
   });
-}
-
-export function updateVendorItem(
-  vendorid: string,
-  id: number,
-  updatedItem: updatedItem
-) {
-  return fetch(`${url}/vendors/${vendorid}/items/${id}/`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(updatedItem),
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`${response.status} ${response.statusText}`);
-      }
-      return response;
-    })
-    .then(response => response.json());
-}
-
-export function deleteVendorItem(vendorid: string, id: number) {
-  return fetch(`${url}/vendors/${vendorid}/items/${id}`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`${response.status} ${response.statusText}`);
-      }
-      return response.json();
-    })
-
-    .catch(error => console.log(error));
-}
-
-export function updateItemQuantity(
-  vendorId: number,
-  itemNum: number,
-  newQuantity: ItemQuantity
-) {
-  return fetch(`${url}/vendors/${vendorId}/items/${itemNum}/`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(newQuantity),
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`${response.status} ${response.statusText}`);
-      }
-      return response;
-    })
-    .then(response => response.json());
-}
-
-export function deleteVendor(vendorID: number) {
-  return fetch(`${url}/vendors/${vendorID}/`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`${response.status} ${response.statusText}`);
-      }
-      return response;
-    })
-    .catch(error => console.log(error));
-}
-
-export function deleteCustomer(customerID: number) {
-  return fetch(`${url}/customers/${customerID}/`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`${response.status} ${response.statusText}`);
-      }
-      return response;
-    })
-    .catch(error => console.log(error));
-}
-
-export function updateCustomerData(userID: number, updatedUserData: any) {
-  console.log('userdata look', updatedUserData);
-  return fetch(`${url}/customers/${userID}/`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(updatedUserData),
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`${response.status} ${response.statusText}`);
-      }
-      return response.json();
-    })
-    .catch(error => console.log(error));
-}
-
-export function updateVendorData(userID: number, updatedUserData: any) {
-  console.log('userdata look', updatedUserData);
-  return fetch(`${url}/vendors/${userID}/`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(updatedUserData),
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`${response.status} ${response.statusText}`);
-      }
-      return response.json();
-    })
-    .catch(error => console.log(error));
 }

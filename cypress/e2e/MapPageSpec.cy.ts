4e25bd46bcf83b87e4d4bdb86985fa0a44391027
Ann Hochworter <ann.hochworter@gmail.com>
2024-03-25 10:21:13 -0600

    Fix endpoints in cypress intercepts

--- cypress/e2e/MapPageSpec.cy.ts
+++ cypress/e2e/MapPageSpec.cy.ts
@@ -5,12 +5,12 @@ describe('should log a customer in and navigate to customer dashboard', () => {
     // Setup intercepts for all customers and all vendors
     cy.intercept(
       'GET',
-      'https://quiet-depths-54407-77a00505f51e.herokuapp.com/customers/',
+      'https://quiet-depths-54407-77a00505f51e.herokuapp.com/api/v1/customers/',
       { statusCode: 200, fixture: 'allCustomersStub' }
     ).as('getAllCustomers');
     cy.intercept(
       'GET',
-      'https://quiet-depths-54407-77a00505f51e.herokuapp.com/vendors/',
+      'https://quiet-depths-54407-77a00505f51e.herokuapp.com/api/v1/vendors/',
       { statusCode: 200, fixture: 'allVendorsStub' }
     ).as('getAllVendors');
 
@@ -27,7 +27,7 @@ describe('should log a customer in and navigate to customer dashboard', () => {
     // Intercept GET for one customer
     cy.intercept(
       'GET',
-      'https://quiet-depths-54407-77a00505f51e.herokuapp.com/customers/*',
+      'https://quiet-depths-54407-77a00505f51e.herokuapp.com/api/v1/customers/*',
       { statusCode: 200, fixture: 'oneCustomerStub' }
     ).as('getOneCustomer');
 
